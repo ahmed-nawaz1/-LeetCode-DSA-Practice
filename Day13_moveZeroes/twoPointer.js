aec73f82815bc1b533bc3c00a2425f6fb787e1e4
ahmed-nawaz1 <unknowncom025@gmail.com>
2026-01-04 03:52:23 +0500

    formatting : gap fix

--- Day13_moveZeroes/twoPointer.js
+++ Day13_moveZeroes/twoPointer.js
@@ -3,18 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-    let i = 0;
-    let j = 0;
+  let i = 0;
+  let j = 0;
 
-    while (i < nums.length) {
-        if (nums[i] !== 0) {
-            if (i !== j) {
-                nums[j] = nums[i];
-                nums[i] = 0;
-                
-            }
-            j++;
-        }
-        i++
+  while (i < nums.length) {
+    if (nums[i] !== 0) {
+      if (i !== j) {
+        nums[j] = nums[i];
+        nums[i] = 0;
+      }
+      j++;
     }
+    i++;
+  }
 };
